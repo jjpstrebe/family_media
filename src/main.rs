376f45a473481fb3696472677ab9c53d42aa5e12
Jared Strebe <jjps@Jareds-MacBook-Pro-2.local>
2023-02-14 12:03:39 -0700

    Got fast working functions for finding all videos and images.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use walkdir::{DirEntry, WalkDir};
 use std::fs;
 
 
-fn is_image(entry: &DirEntry) -> bool {
+fn is_image_slow(entry: &DirEntry) -> bool {
 
     if entry.metadata().unwrap().is_file() {
         infer::is_image(&fs::read(entry.path()).unwrap())
@@ -13,15 +13,27 @@ fn is_image(entry: &DirEntry) -> bool {
 }
 
 
-fn is_image2(entry: &DirEntry) -> bool {
+fn is_image(entry: &DirEntry) -> bool {
 
     if entry.metadata().unwrap().is_file() {
-        let kind = infer::get_from_path(entry.path())
+        let kind_result = infer::get_from_path(entry.path())
             .expect("file read successfully");
-//            .expect("file type is known");
-        match kind {
-            Option => false,
-            _ => kind.mime_type() == "image/jpeg",
+        match kind_result {
+            Some(kind) => kind.mime_type() == "image/jpeg" ||
+                          kind.mime_type() == "image/png" ||
+                          kind.mime_type() == "image/gif" ||
+                          kind.mime_type() == "image/webp" ||
+                          kind.mime_type() == "image/x-canon-cr2" ||
+                          kind.mime_type() == "image/tiff" ||
+                          kind.mime_type() == "image/bmp" ||
+                          kind.mime_type() == "image/heif" ||
+                          kind.mime_type() == "image/avif" ||
+                          kind.mime_type() == "image/vnd.ms-photo" ||
+                          kind.mime_type() == "image/vnd.adobe.photoshop" ||
+                          kind.mime_type() == "image/vnd.microsoft.icon" ||
+                          kind.mime_type() == "image/openraster",
+            // The file type is unknonw.
+            None => false,
         }
     } else {
         false
@@ -30,13 +42,40 @@ fn is_image2(entry: &DirEntry) -> bool {
 }
 
 
-fn is_video(entry: &DirEntry) -> bool {
+fn is_video_slow(entry: &DirEntry) -> bool {
 
     if entry.metadata().unwrap().is_file() {
         infer::is_video(&fs::read(entry.path()).unwrap())
     } else {
         false
     }
+
+}
+
+
+fn is_video(entry: &DirEntry) -> bool {
+
+    if entry.metadata().unwrap().is_file() {
+        let kind_result = infer::get_from_path(entry.path())
+            .expect("file read successfully");
+        match kind_result {
+            Some(kind) => kind.mime_type() == "video/mp4" ||
+                          kind.mime_type() == "video/x-m4v" ||
+                          kind.mime_type() == "video/x-matroska" ||
+                          kind.mime_type() == "video/webm" ||
+                          kind.mime_type() == "video/quicktime" ||
+                          kind.mime_type() == "video/x-msvideo" ||
+                          kind.mime_type() == "video/x-ms-wmv" ||
+                          kind.mime_type() == "video/mpeg" ||
+                          kind.mime_type() == "video/x-flv",
+            // The file type is unknonw.
+            None => false,
+        }
+    } else {
+        false
+    }
+
+
 
 }
 
@@ -64,8 +103,15 @@ fn get_directories(path: &str) {
 fn main() {
 
 //    get_directories("/Users/jjps/Downloads/test");
+// find out if there is video and picture directories
+// if not create them and put all directories in the pictures one
+// create all the pictures directories in the video directory if they don't exist
+// do the same but the other way around
+// find all videos and the pictures directly and move them to the videos directories
+// Do the same but the other way around
+// remove all empty directories
     for entry in WalkDir::new("/Users/jjps/Downloads/test").into_iter().filter_map(|e| e.ok()) {
-        if is_image2(&entry) {
+        if is_video(&entry) {
             println!("{}", entry.path().display());
         }
     }
